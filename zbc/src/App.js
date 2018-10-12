5c41592c9264702a3e81fb538dd017f3f14d0828
b90_100007129645_zbc <b90_100007129645_zbc@git.cloud.tencent.com>
2018-10-12 10:29:32 +0800

    。。。。

--- zbc/src/App.js
+++ zbc/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//传统整个引入
+//import echarts from 'echarts';
+
+//尝试按需加载
+import echarts from './modules/echarts.ness.js';
+
+
+
+
 class App extends Component {
   render() {
     return (
@@ -8,9 +17,44 @@ class App extends Component {
         <header className="App-header">
          
         </header>
+		<main>
+			<div ref="chart" className="chart-container"></div>
+		</main>	
       </div>
     );
   }
+
+
+componentDidMount(){
+
+const chart = echarts.init(this.refs.chart);
+
+const option = {
+	  xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+    }]
+}
+
+
+chart.setOption(option);
+
+
+}
+
+
+
+
+
+
+
 }
 
 export default App;
---
+++ zbc/src/modules/echarts.ness.js
@@ -0,0 +1,7 @@
+
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/line';
+
+
+
+export default echarts;
